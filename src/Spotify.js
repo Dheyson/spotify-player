82999df613c70b251b424708bb433135a369c2be
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-04-14 02:30:31 -0300

    feat: Create page and styles

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper';
 
 const spotify = new SpotifyWrapper({
-  token: 'BQBcqKIVSX26RjD-J3jW5VI7EAAr7JJ5Lh_PXrkktqmLrSEDVESvZ_aKT8SOGLs-CzW-NOZRAHVw_hdv0ctWoUxkiwIZgb9RnEYwTd_aE3HkePoHVedXIjVbog18GlHqFYvfUhgi4p6zIhh4Xb2Lmc1xuLNWw89hp_kL-uvgaRP-xl-oiIP4d6J4',
+  token: 'BQDiw7QsUkCXYLz7QVgW1393Ka0wHmKub65XinkdFwhjjuBdSlpbnjw3goOT8lRc-vQgjqtKiJ7LzyxLiAxedM5efNOwLJaE1xRp3vEIVdm9MVluMHdEVx3TfauF28kmv433eReb1A0-wOG4C0pcPpDXgRnjBx097FGVYPuavvWcax3m9ZHz5JwB',
 });
 
 export default spotify;
--- src/components/AlbumDetails/AlbumDetails.js
+++ src/components/AlbumDetails/AlbumDetails.js
@@ -1,13 +1,15 @@
 function createMarkup(data) {
   return (`
-  <a href="./artist-details.html">
     <img class="album-image" src="${data.images[0].url}" alt="${data.name}">
-  </a>
     <p class="album-title">${data.name}</p>
-  <a href="./artist-details.html">
     <p class="album-artist">${data.artists[0].name}</p>
-  </a>
     <p class="album-counter">${data.tracks.total} Músicas</p>
+    <p class="album-followers">${data.followers.total} Seguidores</p>
+    <p class="album-list">
+    <ul>
+      ${data.map(list => `<li>${list.genres}</li>`)}
+    </ul>
+    </p>
   `);
 }
 
